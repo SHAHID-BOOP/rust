100017367fa84b6cc5977be1f0d1ba2eef7b98c2
SHAHID-BOOP <mohdshahida891@gmail.com>
2025-01-26 20:15:39 +0530

    first commit

---
+++ target/debug/Rust.d
@@ -0,0 +1,1 @@
+/mnt/d/Projects/Rust/target/debug/Rust: /mnt/d/Projects/Rust/src/main.rs
---
+++ target/debug/deps/Rust-fb79f839eca5ffba.d
@@ -0,0 +1,5 @@
+/mnt/d/Projects/Rust/target/debug/deps/Rust-fb79f839eca5ffba: src/main.rs
+
+/mnt/d/Projects/Rust/target/debug/deps/Rust-fb79f839eca5ffba.d: src/main.rs
+
+src/main.rs:
