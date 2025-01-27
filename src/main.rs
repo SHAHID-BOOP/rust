ffccd8d4275e4bfc857d437bb9c15e5f0c3c55a9
SHAHID-BOOP <mohdshahida891@gmail.com>
2025-01-27 22:41:39 +0530

    complete

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ fn main() {
     let ans = is_even(32);
     println!("{}", ans);
 }
-
+// function which returns true if the number is even
 fn is_even(num: i32) -> bool {
     if num % 2==0 {
         return true;
