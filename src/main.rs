5aa2c48a4068c430755234e449ce179efa19d693
SHAHID-BOOP <mohdshahida891@gmail.com>
2025-02-08 20:36:41 +0530

    complete

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ fn main() {
     println!("{}", ans);
 }
 // function which returns true if the number is even
+// and this is rust function
 fn is_even(num: i32) -> bool {
     if num % 2==0 {
         return true;
